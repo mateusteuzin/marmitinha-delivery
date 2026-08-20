e5a715aea1e02b0b22ccf7e8e0147131db309e55
mateusteuzin <ma2664223@gmail.com>
2026-08-20 12:09:19 -0300

    Build Marmitinha digital menu

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+import { MenuApp } from "./MenuApp";
+export const metadata = { title:"Cardápio | Marmitinha Delivery", description:"Cuscuz, tapioca e bebidas. Monte seu pedido e envie pelo WhatsApp." };
+export default function Home(){ return <MenuApp/>; }
